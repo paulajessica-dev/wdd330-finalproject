181dca80851cd1846f4989ca21d833ae0d78a4d5
paulajessica-dev <jessicafldasilva@gmail.com>
2026-02-18 00:21:32 -0300

    run build

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,8 @@ export default defineConfig({
         wish: resolve(__dirname, "src/wish/index.html"),              
         book: resolve(__dirname, "src/book_details/index.html"),
         book_listing: resolve(__dirname, "src/book_listing/index.html"),
+        trending: resolve(__dirname, "src/trending/index.html"),
+        top_rated: resolve(__dirname, "src/top_rated/index.html"),
       },
     },
   },
